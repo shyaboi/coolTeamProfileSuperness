0464484f4d41421b06db16190bf09ac6dd40ac14
Shyaboi <ian.shane.sears@gmail.com>
2020-03-28 22:00:59 -0700

    switch case setup, and node tested, function for new emp roles setup and executing

--- app.js
+++ app.js
@@ -40,6 +40,64 @@ switch (qs.role) {
                 message: "What is github username?"
             },
 
-        ])
+        ]).then(answers => {
+            const eng = new Engineer(answers.name, answers.email, answers.id, qs.role, answers.github)
+            employees.push(eng)
+        })
+        main()
+        
+    break;
+    case "Manager":
+        inquirer.prompt([
+            {
+                name: "name",
+                message: "What is name?"
+
+            },
+            {
+                name: "email",
+                message: "What is email?"
+            },
+            {
+                name: "id",
+                message: "What is  id?"
+            },{
+                name: "officeNumber",
+                message: "What is office number"
+            }
+
+        ]).then(answers => {
+        const man = new Manager(answers.name, answers.email, answers.id, qs.role, answers.officeNumber)
+        employees.push(man)
+        main()
+        })
+        
+            break;
+    case "Intern":
+        inquirer.prompt([
+            {
+                name: "name",
+                message: "What is  name?"
+
+            },
+            {
+                name: "email",
+                message: "What is email?"
+            },
+            {
+                name: "id",
+                message: "What is id?"
+            },
+            {
+                name: "school",
+                message: "What is School??"
+            }
+
+        ]).then(answers => {
+        const intern = new Intern(answers.name, answers.email, answers.id, qs.role, answers.school)
+        employees.push(intern)
+        main()
+        })
+                break;
     }
 }
