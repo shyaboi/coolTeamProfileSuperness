69751705aa7766862f1354dda35b11faff367024
Shyaboi <ian.shane.sears@gmail.com>
2020-03-28 20:55:55 -0700

    inital setup

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,12 @@
+// TODO: Write code to define and export the Employee class
+module.exports = class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {}
+  getId() {}
+  getEmail() {}
+  getRole() {}
+};
